36bd6cc8c60269fe0cca907368698d78f8e0f801
Saumya-Aggarwal <saumyaaggarwal667@gmail.com>
2024-10-18 15:10:27 +0530

    useREF

--- src/components/addTodo.jsx
+++ src/components/addTodo.jsx
@@ -1,19 +1,23 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function AddTodo({ HandleNewItem }) {
-  const [todoName, setTodoName] = useState("");
-  const [dueDate, setDueDate] = useState("");
-  const handleNameChange = (event) => {
-    setTodoName(event.target.value);
-  };
-  const handleDateChange = (event) => {
-    setDueDate(event.target.value);
-  };
-function handleAddButtonClicked(){
-  HandleNewItem(todoName, dueDate);
-  setDueDate("");
-  setTodoName("");
+  // const [todoName, setTodoName] = useState("");
 
+  // const [dueDate, setDueDate] = useState("");
+  const todoName = useRef();
+  const dueDate = useRef();
+  // const handleNameChange = (event) => {
+  //   setTodoName(event.target.value);
+  // };
+  // const handleDateChange = (event) => {
+  //   setDueDate(event.target.value);
+  // };
+  function handleAddButtonClicked() {
+    HandleNewItem(todoName.current.value, dueDate.current.value);
+    todoName.current.value = "";
+    dueDate.current.value= "";
+    // setDueDate("");
+    // setTodoName("");
   }
   return (
     <div className=" container text-center">
@@ -21,13 +25,18 @@ function handleAddButtonClicked(){
         <div className="col-6">
           <input
             type="text"
+            ref={todoName}
             placeholder="Enter your task here..."
-            value={todoName}
-            onChange={handleNameChange}
+            // value={todoName}
+            // onChange={handleNameChange}
           />
         </div>
         <div className="col-4">
-          <input type="date" onChange={handleDateChange} value={dueDate}/>
+          <input type="date" 
+          ref={dueDate}
+          // onChange={handleDateChange} 
+          // value={dueDate} 
+          />
         </div>
         <div className="col-2">
           <button className="btn btn-success" onClick={handleAddButtonClicked}>
